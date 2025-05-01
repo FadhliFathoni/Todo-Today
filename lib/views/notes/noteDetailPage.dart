ae1d83a00f001db1bc29463ce6cf504a593b5b8a
FadhliFathoni <fadhlifathoni62@gmail.com>
2025-05-01 17:42:30 +0700

    update

--- lib/views/notes/noteDetailPage.dart
+++ lib/views/notes/noteDetailPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
-import 'package:todo_today/Component/Text/MyTextStyle.dart';
 import 'package:todo_today/main.dart';
 
 class NoteDetailPage extends StatefulWidget {
@@ -27,14 +26,13 @@ class NoteDetailPage extends StatefulWidget {
 class _NoteDetailPageState extends State<NoteDetailPage> {
   late TextEditingController descController;
   late String originalText;
+  bool isEditMode = false;
 
   @override
   void initState() {
     super.initState();
     originalText = widget.description;
     descController = TextEditingController(text: originalText);
-
-    // Autosave on text change
     descController.addListener(_autoSaveNote);
   }
 
@@ -46,7 +44,6 @@ class _NoteDetailPageState extends State<NoteDetailPage> {
   }
 
   void _autoSaveNote() async {
-    // Save only when the text has changed
     if (descController.text != originalText) {
       await FirebaseFirestore.instance
           .collection('notes')
@@ -60,7 +57,7 @@ class _NoteDetailPageState extends State<NoteDetailPage> {
         'updatedAt': FieldValue.serverTimestamp(),
       });
 
-      originalText = descController.text; // Update originalText after saving
+      originalText = descController.text;
     }
   }
 
@@ -73,32 +70,82 @@ class _NoteDetailPageState extends State<NoteDetailPage> {
         .showSnackBar(const SnackBar(content: Text('Perubahan dibatalkan')));
   }
 
+  Widget _buildFormattedText(String text) {
+    final regex = RegExp(r'==(.+?)==', multiLine: true);
+    final spans = <InlineSpan>[];
+
+    int lastIndex = 0;
+    for (final match in regex.allMatches(text)) {
+      if (match.start > lastIndex) {
+        spans.add(TextSpan(text: text.substring(lastIndex, match.start)));
+      }
+
+      final highlightedText = match.group(1)!;
+
+      spans.add(WidgetSpan(
+        alignment: PlaceholderAlignment.middle,
+        child: GestureDetector(
+          onLongPress: () {
+            Clipboard.setData(ClipboardData(text: highlightedText));
+          },
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 4,
+                  offset: Offset(0, 2),
+                ),
+              ],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Text(
+              highlightedText,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ));
+
+      lastIndex = match.end;
+    }
+
+    if (lastIndex < text.length) {
+      spans.add(TextSpan(text: text.substring(lastIndex)));
+    }
+
+    return RichText(
+      text: TextSpan(
+        style: TextStyle(color: Colors.black, fontSize: 16),
+        children: spans,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(
-          255, 245, 245, 245), // Light grey background for neutral look
+      backgroundColor: Color.fromARGB(255, 245, 245, 245),
       appBar: AppBar(
-        backgroundColor:
-            Color.fromARGB(255, 164, 83, 56), // PRIMARY_COLOR for AppBar
-        elevation: 0, // Removes shadow for clean look
+        backgroundColor: Colors.white,
+        elevation: 0,
         centerTitle: true,
-        leading: BackButton(color: Colors.white),
+        leading: BackButton(color: PRIMARY_COLOR),
         title: Text(
           widget.title,
           style: TextStyle(
-            fontSize: 18,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
+              fontSize: 18, color: PRIMARY_COLOR, fontWeight: FontWeight.bold),
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.undo, color: Colors.white),
+            icon: Icon(Icons.undo, color: PRIMARY_COLOR),
             onPressed: _undoChanges,
           ),
           IconButton(
-            icon: Icon(Icons.copy, color: Colors.white),
+            icon: Icon(Icons.copy, color: PRIMARY_COLOR),
             onPressed: () {
               Clipboard.setData(ClipboardData(text: descController.text));
               ScaffoldMessenger.of(context).showSnackBar(
@@ -106,62 +153,45 @@ class _NoteDetailPageState extends State<NoteDetailPage> {
               );
             },
           ),
+          IconButton(
+            icon: Icon(isEditMode ? Icons.visibility : Icons.edit,
+                color: PRIMARY_COLOR),
+            onPressed: () {
+              setState(() {
+                isEditMode = !isEditMode;
+              });
+            },
+          ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Deskripsi Catatan',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 75, 50, 35), // Deep brown color
-              ),
-            ),
-            SizedBox(height: 16),
-            Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    blurRadius: 6,
-                    spreadRadius: 1,
-                  ),
-                ],
-              ),
-              child: Expanded(
-                // Wrap the TextField inside an Expanded widget to avoid infinite size
-                child: TextField(
-                  cursorColor:
-                      Color.fromARGB(255, 164, 83, 56), // PRIMARY_COLOR
-                  controller: descController,
-                  maxLines: null,
-                  keyboardType: TextInputType.multiline,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Color.fromARGB(
-                        255, 60, 60, 60), // Soft Charcoal text color
-                  ),
-                  decoration: InputDecoration(
-                    hintText: 'Tulis catatan di sini...',
-                    hintStyle: TextStyle(
-                      color: Color.fromARGB(
-                          255, 75, 50, 35), // Deep Brown for hint
-                    ),
-                    border: InputBorder.none,
-                    isCollapsed: true,
-                  ),
-                ),
-              ),
-            ),
-          ],
+      body: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 245, 245, 245),
+          borderRadius: BorderRadius.circular(12),
         ),
+        child: isEditMode
+            ? TextField(
+                cursorColor: Color.fromARGB(255, 164, 83, 56),
+                controller: descController,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromARGB(255, 60, 60, 60),
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Tulis catatan di sini...',
+                  hintStyle: TextStyle(
+                    color: Color.fromARGB(255, 75, 50, 35),
+                  ),
+                  border: InputBorder.none,
+                  isCollapsed: true,
+                ),
+              )
+            : SingleChildScrollView(
+                child: _buildFormattedText(descController.text),
+              ),
       ),
     );
   }
